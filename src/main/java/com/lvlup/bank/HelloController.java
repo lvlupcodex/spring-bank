4cd6b6c23bd52f75b6254544d596e94f692607aa
lvlupcodex <lau.ip.dev@gmail.com>
2025-04-11 20:18:00 +0000

    -m

---
+++ src/main/java/com/lvlup/bank/HelloController.java
@@ -0,0 +1,13 @@
+package com.lvlup.bank;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @GetMapping("/")
+    public String hello() {
+        return "¡Hola Spring Boot desde Codespace guanajo!";
+    }
+}
---
+++ src/main/java/com/lvlup/bank/SpringBankTestApplication.java
@@ -0,0 +1,13 @@
+package com.lvlup.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBankTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBankTestApplication.class, args);
+	}
+
+}
