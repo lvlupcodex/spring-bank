b9372f6523bec5e3daeec638270c609281cf9191
lvlupcodex <lau.ip.dev@gmail.com>
2025-04-12 13:44:20 +0000

    -m

---
+++ src/main/java/com/lvlup/bank/service/CuentaService.java
@@ -0,0 +1,36 @@
+package com.lvlup.bank.service; 
+import com.lvlup.bank.model.Cuenta;
+import com.lvlup.bank.repository.CuentaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.list; 
+import java.util.Optional;
+
+@Service 
+public class CuentaService{
+    //Private final lo que es (clase) y el objeto
+    private final CuentaRepository cuentaRepository; 
+
+    public CuentaService(CuentaRepository cuentaRepository){
+        this.cuentaRepository = cuentaRepository;
+    }
+
+    //Me devuelve un ARRAY de Cuentas
+    
+    public List<Cuenta> obtenerTodas(){
+        return cuentaRepository.findAll();
+    }
+
+    public Optional<Cuenta> obtenerPorId(Long id){
+        return cuentaRepository.findById(id);
+    }
+    //EL ORDEN: Tipo de objeto y objeto
+    //NECESITO DE TIPO Cuenta un objeto cuenta
+    //momento mayus
+    public Cuenta crear(Cuenta cuenta){
+        return cuentaRepository.save(cuenta);
+    }
+    public void eliminar(Long id){
+        cuentaRepository.deleteById(id);
+    }
+}
